e869a081937e79ab416de0a373c3fe189d212276
Michail Borisenko <bmaa@yandex.ru>
2024-12-12 21:02:03 +0300

    feature/RS-17-create-comment-db bma 2

--- migration/comments.sql
+++ migration/comments.sql
@@ -2,13 +2,13 @@
 По каждой жалобе записываем историю в отдельную таблицу.
 Исторические данные удаляется спустя год, основная таблица - без очистки.
 
-Согласно ТЗ следует отражать uuid жалобы в названии таблицы.
+Согласно ТЗ следует отражать uuid жалобы в названии таблицы
 */
 
-CREATE TABLE IF NOT EXISTS comment%s (
+CREATE TABLE IF NOT EXISTS comments (
 	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	user_uuid UUID,
-	comment TEXT,
+	comment VARCHAR(255),
 	created_at DATE NOT NULL DEFAULT CURRENT_DATE,
 	updated_at DATE NOT NULL DEFAULT CURRENT_DATE
 );
@@ -17,12 +17,15 @@ CREATE TABLE IF NOT EXISTS comment%s (
 Общий вариант БД комментариев для хранения ВСЕХ коментариев ко всем жалобам. 
 Этот вариант может упростить поиск информации в комментрариях, т е не нужно 
 пробегать по всем файлам комментов для поиска.
+Во второй таблице дополнительное поле compliance_uuid, т к таблица целиковая на все
+жалобы. Для различий по жалобам это поле и нужно. Эта таблица не удовлетворяем требованиям ТЗ, 
+но возможно облегчит поиск нужной информации в комментариях. Если это лишнее, то таблица будет удалена.
 */
 CREATE TABLE IF NOT EXISTS comments (
 	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	compliance_uuid UUID,
+	FOREIGN KEY (compliance_id) REFERENCES REPORTS (id),
 	user_uuid UUID,
-	comment TEXT,
+	comment VARCHAR(255),
 	created_at DATE NOT NULL DEFAULT CURRENT_DATE,
 	updated_at DATE NOT NULL DEFAULT CURRENT_DATE
 );
