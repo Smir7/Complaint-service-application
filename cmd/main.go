677824309e3538d35ca7add051a545b275b4d42f
Listopadov Denis <153854599+maliven1@users.noreply.github.com>
2024-12-16 12:07:00 +0300

    logger package (#44)
    * logger package
    
    * вынес в переменную slog.String

--- cmd/main.go
+++ cmd/main.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"complaint_service/internal/config"
+	l "complaint_service/internal/logger"
 	"log/slog"
-	"os"
 
 	"github.com/gofiber/fiber"
 )
 
 const port = ":8080"
-const (
-	envLocal = "local"
-	envProd  = "prod"
-)
 
 func main() {
 	/*
 		Далее передаем в наши ручки log *slog.Logger и с ним работаем.
 		Для удобства, в каждой ручке можно использовать такую конструкцию, чтоб дальше подтягивалась информация.
-		log := log.With(
+		op:="handlers.GetUser"
+		log := slogger.Log.log.With(
 			slog.String("где вылезла ошибка", op),
 		)
 	*/
+
 	cfg := config.NewConfig()
-	log := setupLogger(cfg.Env)
-	log.Info("Starting project", slog.String("env", cfg.Env))
-	log.Debug("debug messages are enabled", slog.String("env", cfg.Env))
+	str := slog.String("env", cfg.Env)
+	l.SetupLogger(cfg.Env)
+	l.Log.Info("Starting project", str)
+	l.Log.Debug("debug messages are enabled", str)
+
 	/*
 		Инициализируем БД. И коннект прокидываем в CreateComplaintsRepository
 		complaintsRepository := repository.CreateComplaintsRepository(db)
@@ -43,22 +43,8 @@ func main() {
 		Подключаем роуты. Прокидываем инициализированные хендлеры complaintsHandler
 		routes.Complaints(app, complaintsHandler)
 	*/
-	log.Info("The server is running", slog.String("port", port))
+	l.Log.Info("The server is running", slog.String("port", port))
 	if err := app.Listen(port); err != nil {
-		log.Error("Server startup error: %v", err)
+		l.Log.Error("Server startup error: %v", err)
 	}
-}
-
-func setupLogger(env string) *slog.Logger {
-	var log *slog.Logger
-
-	switch env {
-	case envLocal:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-
-	case envProd:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-	}
-	return log
-
 }
